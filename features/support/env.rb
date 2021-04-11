716bf50979fb6b641a22a4c13f84d7275c751410
David Halison <halison700@gmail.com>
2021-04-11 18:16:28 -0300

    [UPDATE] ajustando o driver do chrome headless

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,8 +2,20 @@ require 'capybara'
 require 'capybara/cucumber'
 require 'selenium-webdriver'
 
+Capybara.register_driver :selenium_chrome_headless do |app|
+
+    Capybara::Selenium::Driver.load_selenium
+    browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+        opts.args << "--headless"
+        opts.args << "--disable-gpu" if Gem.win_platform?
+        opts.args << "--no-sandbox"
+        opts.args << "--disable-site-isolation-trials"
+    end
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+end
+
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome
+    config.default_driver = :selenium_chrome_headless
     config.default_max_wait_time = 10
     config.app_host = 'http://localhost:3000'
 end
