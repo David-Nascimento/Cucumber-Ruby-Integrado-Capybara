583685b610d8c0360b7473098e21c64bf49dba78
David Halison <halison700@gmail.com>
2021-04-12 19:49:09 -0300

    [UPDATE] alterado script de instalacao do sonar

--- scripts/install-sonar-scanner.sh
+++ scripts/install-sonar-scanner.sh
@@ -5,19 +5,19 @@ echo "Downloading sonar-scanner....."
 if [ -d "/tmp/sonar-scanner-cli-3.2.0.1227-linux.zip" ];then
     sudo rm /tmp/sonar-scanner-cli-3.2.0.1227-linux.zip
 fi
-wget -q https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.2.0.1227-linux.zip
+wget -q https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.6.0.2311-linux.zip
 echo "Download completed."
 
 echo "Unziping downloaded file..."
-unzip sonar-scanner-cli-3.2.0.1227-linux.zip
+unzip sonar-scanner-cli-4.6.0.2311-linux.zip
 echo "Unzip completed."
-rm sonar-scanner-cli-3.2.0.1227-linux.zip
+rm sonar-scanner-cli-4.6.0.2311-linux.zip
 
 echo "Installing to opt..."
 if [ -d "/var/opt/sonar-scanner-3.2.0.1227-linux" ];then
-    sudo rm -rf /var/opt/sonar-scanner-3.2.0.1227-linux
+    sudo rm -rf /var/opt/sonar-scanner-cli-4.6.0.2311-linux
 fi
-sudo mv sonar-scanner-3.2.0.1227-linux /var/opt
+sudo mv sonar-scanner-cli-4.6.0.2311-linux /var/opt
 
 echo "Installation completed successfully."
 
